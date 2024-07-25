31258a5b5db3858b33c3709bbbbf726fa331327c
galatea <157182004+gggalatea@users.noreply.github.com>
2024-07-26 07:04:45 +1200

    Solve 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, currentNum) => total + currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, currentNum) => product * currentNum);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let product = 1;
+  for (let i = 1; i <= num; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
