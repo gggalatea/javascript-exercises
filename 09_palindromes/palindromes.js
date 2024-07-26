8cbb72fab00aef2a792734d5996e24e08fecaa78
galatea <157182004+gggalatea@users.noreply.github.com>
2024-07-27 07:21:51 +1200

    Solve 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // other method
+    // const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const filteredString = string
+                            .toLowerCase()
+                            .split(/\W+/)
+                            .join("");
+
+                            // .toLowerCase()
+                            // .split("")
+                            // .filter((character) => alphanumerical.includes(character))
+                            // .join("");
+    const reversedFilteredString = filteredString
+                                    .split("")
+                                    .reverse()
+                                    .join("");
+    return filteredString === reversedFilteredString;
 };
 
 // Do not edit below this line
