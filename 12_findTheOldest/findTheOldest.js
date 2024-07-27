2839ea02c7773b7d294f12c45eefcd95a43a9660
galatea <157182004+gggalatea@users.noreply.github.com>
2024-07-27 12:25:52 +1200

    Solve 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function getAge(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = (new Date()).getFullYear();
+    }
+    return deathYear - birthYear;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestPersonAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestPersonAge > currentPersonAge ? oldestPerson : currentPerson;
+    });
 };
 
 // Do not edit below this line
